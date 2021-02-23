667be2344c9271536b0d6f0a82101b9f42379c1d
saad-el-harchi <44896216+saad-el-harchi@users.noreply.github.com>
2021-02-24 00:35:22 +0100

    Update RetrofitInstance.java

--- app/src/main/java/com/example/passwordmanager/dialog/RetrofitInstance.java
+++ app/src/main/java/com/example/passwordmanager/dialog/RetrofitInstance.java
@@ -6,19 +6,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
 
     private static Retrofit retrofit;
-    private static String prefix;
-    private static String base_url = "https://api.pwnedpasswords.com/range/";
+    private static String base_url = "https://jsonplaceholder.typicode.com";
 
-    public static String getPrefix(){
-        return prefix;
-    }
-    public static void setPrefix(String p){
-        prefix = p;
-    }
     public static Retrofit getRetrofitInstance(){
         if(retrofit == null){
             retrofit = new retrofit2.Retrofit.Builder().
-                    baseUrl(base_url+getPrefix()).
+                    baseUrl(base_url).
                     addConverterFactory(GsonConverterFactory.create()).
                     build();
         }
